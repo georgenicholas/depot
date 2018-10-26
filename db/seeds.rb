7ac3e4148c939b8c58d72488f2c2f8b9c953efe6
georgenicholas <george@dringo.io>
2018-10-26 13:38:36 -0700

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(title: 'automated test data',
+  description: 'this is all test data created in the seeds.rb file',
+  image_url: 'test.jpg',
+  price: 29)
