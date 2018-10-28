dadde2687b659767f77fb4a602ba7882a9673041
georgenicholas <george@dringo.io>
2018-10-28 15:27:08 -0700

    test done

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product attributes must not be empty" do
+    product = Product.new(title:  "my book title",
+                          description:  "yyy",
+                          image_url:    "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 2
+    product.valid?
+  end
+
+  test "product titles must be unique" do
+    # fixtures :products
+    product = Product.new(title:  products(:ruby).title,
+                          description:  "yyy",
+                          image_url:    "zzz.jpg")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
